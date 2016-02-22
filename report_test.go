c818d3d7ba24a2931d5456ef2554dbbe58f8ce76
Dan Condomitti <dan@interval.io>
2016-02-22 14:53:36 -0500

    Add l3 fields.

--- report_test.go
+++ report_test.go
@@ -14,16 +14,15 @@ type Report struct {
 }
 
 type CspReport struct {
-	DocumentUri        string `json:"document-uri"`
-	Referrer           string `json:"referrer"`
-	ViolatedDirective  string `json:"violated-directive"`
-	EffectiveDirective string `json:"effective-directive"`
-	OriginalPolicy     string `json:"original-policy"`
-	BlockedUri         string `json:"blocked-uri"`
-	SourceFile         string `json:"source-file"`
-	LineNumber         int    `json:"line-number"`
-	ColumnNumber       int    `json:"column-number"`
-	StatusCode         int    `json:"status-code"`
+	DocumentUri       string `json:"document-uri"`
+	Referrer          string `json:"referrer"`
+	ViolatedDirective string `json:"violated-directive"`
+	OriginalPolicy    string `json:"original-policy"`
+	BlockedUri        string `json:"blocked-uri"`
+	SourceFile        string `json:"source-file"`
+	LineNumber        int    `json:"line-number"`
+	ColumnNumber      int    `json:"column-number"`
+	StatusCode        int    `json:"status-code"`
 }
 
 func ReportHandler(rw http.ResponseWriter, req *http.Request) {
